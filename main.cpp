6b4f1379b6650fce88843746240ba7ba635277a8
varaljai <varaljaivikotr@gmail.com>
2026-03-08 20:19:38 +0100

    Better collision. :O

--- main.cpp
+++ main.cpp
@@ -36,8 +36,24 @@ public:
         return Vec2(this->x * other, this->y * other);
     }
 
+    Vec2 operator/(const float &other) {
+        return Vec2(this->x / other, this->y / other);
+    }
+
     float lenght_squared() {
         return this->x*this->x + this->y*this->y;
+    }
+
+    float lenght() {
+        return sqrt(this->lenght_squared());
+    }
+
+    Vec2 normalize() {
+        return *this / this->lenght();
+    }
+
+    float dot(const Vec2 &other) {
+        return this->x * other.x + this->y * other.y;
     }
 };
 
@@ -77,9 +93,17 @@ public:
             this->position.y = 0. + this->size / 2.;
         }
     }
-    void check_collision_with_balls(Ball &other){
+    void check_collision_with_a_ball_and_resolve(Ball &other){
         if (pow(this->size / 2. + other.size / 2.,2) > (this->position - other.position).lenght_squared() ) {
-            swap(this->velocity,other.velocity);
+            Vec2 collision_normal = (this->position - other.position).normalize();
+            Vec2 relative_velocity = (this->velocity - other.velocity);
+            float velocity_along_normal = relative_velocity.dot(collision_normal) ;
+            if (velocity_along_normal >= 0.) {
+                return;
+            }
+            Vec2 impulse = collision_normal * velocity_along_normal;
+            this->velocity = this->velocity - impulse;
+            other.velocity = other.velocity + impulse;
         }
 
     }
@@ -118,20 +142,21 @@ public:
 
     void event(const event &ev) {
         if (ev.type == ev_timer) {
-            for (int i = 0; this->model.balls.size() > i; i++) {
+            for (int i=0; this->model.balls.size() > i; i++) {
                 Ball &a_ball = this->model.balls[i];
-                a_ball.update((float)FRAME_TIME / 1000.); // TODO: el lehet e kérni az eltelt időt?
-
+                a_ball.update((float)FRAME_TIME /1000.); // TODO: el lehet-e kérni az eltelt időt?
 
                 for (int j = i+1; this->model.balls.size() > j; j++) {
                     Ball &b_ball = this->model.balls[j];
-                    a_ball.check_collision_with_balls(b_ball);
+                    a_ball.check_collision_with_a_ball_and_resolve(b_ball);
+
                 }
-
             }
-        }
 
+
+        }
     }
+
     void draw() {
         clear();
 
@@ -143,8 +168,6 @@ public:
     }
 
 };
-
-
 
 int main() {
     App app = App();
